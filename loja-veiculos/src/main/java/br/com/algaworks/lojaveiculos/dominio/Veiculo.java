bba5d8299c39fd40449ec217f3f35e7d59822bc9
Lucas Souza <lhenriquesouza00@gmail.com>
2020-10-17 08:29:08 -0300

    2.2. Criando o Domain Model

---
+++ loja-veiculos/src/main/java/br/com/algaworks/lojaveiculos/dominio/Veiculo.java
@@ -0,0 +1,60 @@
+package br.com.algaworks.lojaveiculos.dominio;
+
+import java.math.BigDecimal;
+
+public class Veiculo {
+    private Long codigo;
+    private String fabricante;
+    private String modelo;
+    private Integer anoFabricacao;
+    private Integer anoModelo;
+    private BigDecimal valor;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public Integer getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(Integer anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
